26e35d66c2e5622ec1d555bffc0aaf8292f74c1b
quentin <girard.quentin359@gmail.com>
2023-05-20 14:05:35 +0200

    Setup missing uniqueness on various models

--- server/spec/models/company_spec.rb
+++ server/spec/models/company_spec.rb
@@ -9,9 +9,18 @@ RSpec.describe Company, type: :model do
     expect(FactoryBot.build(:company, immatriculation: nil)).to_not be_valid
   end
 
-  it 'should have a valid immatriculation' do
-    expect(FactoryBot.build(:company, immatriculation: '012345678')).to be_valid
-    expect(FactoryBot.build(:company, immatriculation: '01234567')).to_not be_valid
-    expect(FactoryBot.build(:company, immatriculation: '0123456789')).to_not be_valid
+  describe 'immatriculation' do
+    it 'should be a 9 digit code' do
+      expect(FactoryBot.build(:company, immatriculation: '012345678')).to be_valid
+      expect(FactoryBot.build(:company, immatriculation: '01234567')).to_not be_valid
+      expect(FactoryBot.build(:company, immatriculation: '0123456789')).to_not be_valid
+    end
+
+    it 'should be unique' do
+      company = FactoryBot.create(:company)
+      expect(
+        FactoryBot.build(:company, immatriculation: company.immatriculation)
+      ).to_not be_valid
+    end
   end
 end
--- server/spec/models/pvmes_form_spec.rb
+++ server/spec/models/pvmes_form_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe PvmesForm, type: :model do
     expect(FactoryBot.build(:pvmes_form, customer: nil)).to_not be_valid
     expect(FactoryBot.build(:pvmes_form, installation: nil)).to_not be_valid
   end
+
+  it 'should be unique with company, customer and installation' do
+    pvmes_form = FactoryBot.create(:pvmes_form)
+    expect(
+      FactoryBot.build(
+        :pvmes_form, company: pvmes_form.company,
+        customer: pvmes_form.customer,
+        installation: pvmes_form.installation
+      )
+    ).to_not be_valid
+  end
 end
