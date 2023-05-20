8edc12189d9543b78811e7507722f19530ba45ad
quentin <girard.quentin359@gmail.com>
2023-05-20 18:42:11 +0200

    Remove useless comment

--- server/app/controllers/pvmes_forms_controller.rb
+++ server/app/controllers/pvmes_forms_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# curl -d '{"key1":"value1", "key2":"value2"}' -H "Content-Type: application/json" -X POST http://localhost:3000/pvmes_forms
-
 class PvmesFormsController < ApplicationController
   def create
     payload = JSON.parse(params[:body], symbolize_names: true)
