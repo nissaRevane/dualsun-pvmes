d65e2675ffa721e904c4d30c242e6705a43ea181
nissaRevane <53036793+nissaRevane@users.noreply.github.com>
2023-05-20 18:42:41 +0200

    Api endpoint to create pvmes forms (#4)
    * Create endpoint for pvmes forms post requests
    
    * Adapt constructor
    
    * Remove useless comment

---
+++ server/app/controllers/pvmes_forms_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class PvmesFormsController < ApplicationController
+  def create
+    payload = JSON.parse(params[:body], symbolize_names: true)
+    constructor = PvmesFormConstructor.new(payload)
+    created_id = constructor.call
+
+    if created_id
+      render json: { id: created_id }, status: :created
+    else
+      render json: { errors: constructor.errors }, status: :unprocessable_entity
+    end
+  end
+end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,2 +1,3 @@
 Rails.application.routes.draw do
+  post '/pvmes_forms', to: 'pvmes_forms#create'
 end
