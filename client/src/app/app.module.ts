1b362a86143a09299cf7f0e1223a0eadbd713e07
quentin <girard.quentin359@gmail.com>
2023-05-21 10:53:56 +0200

    Setup ngrx store

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,10 @@ import { PvmesFormComponent } from './pvmes-form/pvmes-form.component';
 import { CompanyComponent } from './pvmes-form/company/company.component';
 import { CustomerComponent } from './pvmes-form/customer/customer.component';
 import { InstallationComponent } from './pvmes-form/installation/installation.component';
+import { StoreModule } from '@ngrx/store';
+
+import { pvmesFormReducer } from './state/pvmes-form-reducer';
+import { metaReducers } from './state/pvmes-form-reducer';
 
 @NgModule({
   declarations: [
@@ -33,6 +37,14 @@ import { InstallationComponent } from './pvmes-form/installation/installation.co
     FormsModule,
     ReactiveFormsModule,
     NgbModule,
+    StoreModule.forRoot(
+      {
+        pvmesForm: pvmesFormReducer,
+      },
+      {
+        metaReducers: metaReducers,
+      }
+    ),
   ],
   providers: [{ provide: NgbDateAdapter, useClass: NgbDateNativeUTCAdapter }],
   bootstrap: [AppComponent],
